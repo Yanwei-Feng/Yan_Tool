33976a0ace6ba1a3a28e24ad38111f284b54f462
冯砚伟 <fengyanwei@jd.com>
2020-03-17 09:38:36 +0800

    提交

---
+++ Yan_Tool.framework/Headers/CYSayHi.h
@@ -0,0 +1,17 @@
+//
+//  CYSayHi.h
+//  Yan_Tool
+//
+//  Created by 冯砚伟 on 2020/3/17.
+//  Copyright © 2020 冯砚伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYSayHi : NSObject
+-(void)sayHi;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Yan_Tool.framework/Headers/Yan_Tool.h
@@ -0,0 +1,19 @@
+//
+//  Yan_Tool.h
+//  Yan_Tool
+//
+//  Created by 冯砚伟 on 2020/3/17.
+//  Copyright © 2020 冯砚伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Yan_Tool.
+FOUNDATION_EXPORT double Yan_ToolVersionNumber;
+
+//! Project version string for Yan_Tool.
+FOUNDATION_EXPORT const unsigned char Yan_ToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Yan_Tool/PublicHeader.h>
+
+
